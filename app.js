d7f31af8c7b342a7d1afb3c7c621969657e61eb7
Joy <jzembeuk@gmail.com>
2024-08-14 02:07:58 -0500

    commit

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const adventurer = {
     health: 10,
     inventory: ["sword", "potion", "artifact"]
     }
+    const target = adventurer.inventory[0]
+    console.log(target)
+//access Robin’s inventory using a combination of dot notation and
+//square bracket syntax
+//const target = obj.inventory
+
+//console.log(target)
